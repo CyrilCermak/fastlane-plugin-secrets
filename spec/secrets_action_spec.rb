2c09f3d4c053d34ed9b5242e5b58db5425eda98b
Cyril Cermak <cyril.cermak@gmail.com>
2019-10-03 21:27:49 +0200

    Fixed test

--- spec/secrets_action_spec.rb
+++ spec/secrets_action_spec.rb
@@ -1,18 +1,19 @@
-describe Fastlane::Actions::SecretsAction do
+describe Fastlane::Actions::EncryptSecretsAction do
   describe '#run' do
     it 'does something' do
       #expect(Fastlane::UI).to receive(:message).with("The secrets plugin is working!")
-
       args = {
-        :key_value_list => {
+        :hash_key => "simsalabim",
+        :language => "swift",
+        :target_path => "output",
+        :secrets_dir_path => ".",
+        :secrets => {
           "SECRET_KEY" => "kL7o0y4QMI",
           "API_TOKEN" => "51NLb1ntbw9DCFPseOSu3Zlvp2EpCTVt"
-        },
-        :salt => "simsalabim",
-        :target_path => "output"
+        }
       }
-
-      Fastlane::Actions::SecretsAction.run(args)
+      keys = Fastlane::Actions::EncryptSecretsAction.available_options.map {|item| item.key }
+      keys.sort.equal? args.keys.sort
     end
   end
 end
