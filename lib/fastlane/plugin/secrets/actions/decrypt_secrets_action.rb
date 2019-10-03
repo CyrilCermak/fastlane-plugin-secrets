61b734474e5e290df872499efd50588dc15bd347
Cyril Cermak <cyril.cermak@gmail.com>
2019-10-03 14:48:22 +0200

    Implemented encrypt action

---
+++ lib/fastlane/plugin/secrets/actions/decrypt_secrets_action.rb
@@ -0,0 +1,77 @@
+require 'fastlane/action'
+require 'mobile-secrets'
+require_relative '../helper/secrets_helper'
+
+module Fastlane
+  module Actions
+    class DecryptSecretsAction < Action
+      def self.run(params)
+        secrets_path = params[:file_path]
+        password = params[:password]
+        private_key_path = params[:private_key_path]
+        target_path = "#{Dir.pwd}/#{params[:target_path]}/secrets.swift"
+        tmp_decrypted_secrets_file = "/tmp/secrets"
+
+        clean tmp_decrypted_secrets_file
+        if private_key_path && password
+          sh("gpg", "-v", "--pinentry-mode", "loopback", "--passphrase", password, "--import", private_key_path)
+          sh("gpg", "-a", "--pinentry-mode", "loopback", "--passphrase", password, "--output", tmp_decrypted_secrets_file, "--decrypt", secrets_path)
+        elsif password then
+          sh("gpg", "-a", "--pinentry-mode", "loopback", "--passphrase", password, "--output", tmp_decrypted_secrets_file, "--decrypt", secrets_path)
+        else
+          sh("gpg", "--output", tmp_decrypted_secrets_file, "--decrypt", secrets_path)
+        end
+
+        secrets_handler = MobileSecrets::SecretsHandler.new
+        yml_config = File.read tmp_decrypted_secrets_file
+        bytes = secrets_handler.process_yaml_config yml_config
+        secrets_handler.inject_secrets bytes, target_path
+        clean tmp_decrypted_secrets_file
+      end
+
+      def self.description
+        "Securely store secrets in source code"
+      end
+
+      def self.authors
+        ["Cyril Cermak, Jörg Nestele"]
+      end
+
+      def self.details
+        # Optional:
+        ""
+      end
+
+      def self.clean file_path
+        File.delete(file_path) if File.exist?(file_path)
+      end
+
+      def self.available_options
+        [
+          FastlaneCore::ConfigItem.new(key: :file_path,
+                                       description: "Path to the encrypted secrets file",
+                                       is_string: true),
+          FastlaneCore::ConfigItem.new(key: :target_path,
+                                       description: "Output path for the auto generated source file",
+                                       is_string: true),
+          FastlaneCore::ConfigItem.new(key: :password,
+                                       description: "Password to open the GPG secrets file",
+                                       is_string: true,
+                                       optional: true),
+          FastlaneCore::ConfigItem.new(key: :private_key_path,
+                                       description: "Path to a private key for GPG",
+                                       is_string: true,
+                                       optional: true)
+        ]
+      end
+
+      def self.is_supported?(platform)
+        # Adjust this if your plugin only works for a particular platform (iOS vs. Android, for example)
+        # See: https://docs.fastlane.tools/advanced/#control-configuration-by-lane-and-by-platform
+        #
+        # [:ios, :mac, :android].include?(platform)
+        [:ios, :mac].include?(platform)
+      end
+    end
+  end
+end
---
+++ lib/fastlane/plugin/secrets/actions/encrypt_secrets_action.rb
@@ -0,0 +1,67 @@
+require 'fastlane/action'
+require 'mobile-secrets'
+require 'yaml'
+
+require_relative '../helper/secrets_helper'
+
+module Fastlane
+  module Actions
+    class EncryptSecretsAction < Action
+      def self.run(params)
+        hash_key = params[:hash_key]
+        language = params[:language]
+        secrets_dir_path = params[:secrets_dir_path]
+        secrets_dict = params[:secrets]
+        target_path = "#{Dir.pwd}/#{params[:target_path]}/secrets.swift"
+        config_yml = {"MobileSecrets" => {"hashKey"=>hash_key,"language"=>language, "secrets"=>secrets_dict}}.to_yaml
+        MobileSecrets::SecretsHandler.new.encrypt "#{secrets_dir_path}/secrets.gpg", config_yml, secrets_dir_path
+      end
+
+      def self.description
+        "Securely store secrets in source code"
+      end
+
+      def self.authors
+        ["Cyril Cermak, Jörg Nestele"]
+      end
+
+      def self.details
+        # Optional:
+        ""
+      end
+
+      def self.clean file_path
+        File.delete(file_path) if File.exist?(file_path)
+      end
+
+      def self.available_options
+        [
+          FastlaneCore::ConfigItem.new(key: :hash_key,
+                                       description: "Key that will be used to hash the secrets using XOR technique",
+                                       is_string: true),
+          FastlaneCore::ConfigItem.new(key: :language,
+                                       description: "Source code language, currently supported - [swift]",
+                                       is_string: true),
+          FastlaneCore::ConfigItem.new(key: :target_path,
+                                       description: "Target path of where the secrets will be created",
+                                       is_string: true),
+          FastlaneCore::ConfigItem.new(key: :secrets_dir_path,
+                                        description: "Path to a directory where .gpg was initialized",
+                                        is_string: true),
+          FastlaneCore::ConfigItem.new(key: :secrets,
+                                       description: "Key-value dictionary of secrets",
+                                       type: Hash,
+                                       optional: true)
+        ]
+      end
+
+      def self.is_supported?(platform)
+        # Adjust this if your plugin only works for a particular platform (iOS vs. Android, for example)
+        # See: https://docs.fastlane.tools/advanced/#control-configuration-by-lane-and-by-platform
+        #
+        # [:ios, :mac, :android].include?(platform)
+        [:ios, :mac].include?(platform)
+      end
+    end
+  end
+end
