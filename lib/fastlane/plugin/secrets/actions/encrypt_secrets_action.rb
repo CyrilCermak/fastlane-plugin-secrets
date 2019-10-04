27d450836180b93da2a8e3b356aac1a04bb77e3c
Cyril Cermak <cyril.cermak@gmail.com>
2019-10-04 11:55:12 +0200

    Removed unused variable

--- lib/fastlane/plugin/secrets/actions/encrypt_secrets_action.rb
+++ lib/fastlane/plugin/secrets/actions/encrypt_secrets_action.rb
@@ -10,7 +10,6 @@ module Fastlane
         language = params[:language]
         secrets_dir_path = params[:secrets_dir_path]
         secrets_dict = params[:secrets]
-        target_path = "#{Dir.pwd}/#{params[:target_path]}/secrets.swift"
         config_yml = {"MobileSecrets" => {"hashKey"=>hash_key,"language"=>language, "secrets"=>secrets_dict}}.to_yaml
 
         MobileSecrets::SecretsHandler.new.encrypt "#{secrets_dir_path}/secrets.gpg", config_yml, secrets_dir_path
@@ -40,9 +39,6 @@ module Fastlane
                                        is_string: true),
           FastlaneCore::ConfigItem.new(key: :language,
                                        description: "Source code language, currently supported - [swift]",
-                                       is_string: true),
-          FastlaneCore::ConfigItem.new(key: :target_path,
-                                       description: "Target path of where the secrets will be created",
                                        is_string: true),
           FastlaneCore::ConfigItem.new(key: :secrets_dir_path,
                                         description: "Path to a directory where .gpg was initialized",
